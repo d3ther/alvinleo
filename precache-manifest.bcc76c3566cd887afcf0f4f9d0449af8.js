c88a1eeb816a791b1b5ed0c4f7d6559770ec84fb
d3ther <alvinleonardo2010@gmail.com>
2020-04-16 14:27:15 +0700

    Updates

---
+++ precache-manifest.bcc76c3566cd887afcf0f4f9d0449af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090b4ab14c07a2082312e762459f6790",
+    "url": "/alvinleo/index.html"
+  },
+  {
+    "revision": "8de6e234a2fcba95b172",
+    "url": "/alvinleo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0ae8ba1e5d9473fb76f",
+    "url": "/alvinleo/static/js/2.333adc53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alvinleo/static/js/2.333adc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de6e234a2fcba95b172",
+    "url": "/alvinleo/static/js/main.4126c10e.chunk.js"
+  },
+  {
+    "revision": "1106a3349183da06b2c3",
+    "url": "/alvinleo/static/js/runtime-main.606ee494.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alvinleo/static/media/logo.5d5d9eef.svg"
+  }
+]);
