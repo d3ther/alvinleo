1b9c9929263b1fdfeab56ba34b026a083ee5b8a1
d3ther <alvinleonardo2010@gmail.com>
2020-04-16 16:27:42 +0700

    another one

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-          Learn React 12np
+          Learn React 12npaa
         </a>
       </header>
     </div>
